c4a472ad73b1665fba5d9c89b99655345fdac65b
Jake Dudum <jakedudum@gmail.com>
2020-11-03 19:38:18 -0800

    Refactored categories.php code by moving php code that added new categories to a new file called functions.php which is included in admin_header.php

--- admin/categories.php
+++ admin/categories.php
@@ -19,26 +19,8 @@
                     <!-- Add Category Form -->
                     <div class="col-xs-6">
 
-                        <?php
+                        <?php insert_categories() ?>
 
-                        if (isset($_POST['submit'])) {
-                            $cat_title = $_POST['cat_title'];
-
-                            if ($cat_title == "" || empty($cat_title)) {
-                                echo "This field should not be empty.";
-                            } else {
-                                $query = "INSERT INTO categories(cat_title) ";
-                                $query .= "VALUE('{$cat_title}') ";
-
-                                $create_category_query = mysqli_query($connection, $query);
-
-                                if (!$create_category_query) {
-                                    die("QUERY FAILED" . mysqli_error($connection));
-                                }
-                            }
-                        }
-
-                        ?>
                         <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat-title">Add Category</label>
