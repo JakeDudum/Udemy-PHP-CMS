2619579133b1efdd4a8c493480140991d41d2bb5
Jake Dudum <jakedudum@gmail.com>
2020-11-05 19:01:57 -0800

    Created edit_post.php and copied over form from add_post.php to be used again for editing a post.

--- admin/posts.php
+++ admin/posts.php
@@ -29,8 +29,8 @@
                         case 'add_post';
                             include "includes/add_post.php";
                             break;
-                        case '100';
-                            echo "NICE 100";
+                        case 'edit_post';
+                            include "includes/edit_post.php";
                             break;
                         default:
                             include "includes/view_all_posts.php";
