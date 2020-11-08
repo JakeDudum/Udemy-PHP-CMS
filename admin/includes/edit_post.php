28680f6de80fe7d40c71f3961acc1b2a170bc78a
Jake Dudum <jakedudum@gmail.com>
2020-11-07 17:10:06 -0800

    Updated edit_post.php to update the database with new values and fixed several name typos in the form.

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -19,6 +19,42 @@ if (isset($_GET['p_id'])) {
         $post_comment_count = $row['post_comment_count'];
         $post_date = $row['post_date'];
     }
+
+    if (isset($_POST['update_post'])) {
+        $post_author = $_POST['post_author'];
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category'];
+        $post_status = $_POST['post_status'];
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
+        if (empty($post_image)) {
+            $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+            $select_image = mysqli_query($connection, $query);
+
+            while ($row = mysqli_fetch_array($select_image)) {
+                $post_image = $row['post_image'];
+            }
+        }
+
+        $query = "UPDATE posts SET ";
+        $query .= "post_title = '{$post_title}', ";
+        $query .= "post_author = '{$post_author}', ";
+        $query .= "post_date = now(), ";
+        $query .= "post_category_id = '{$post_category_id}', ";
+        $query .= "post_status = '{$post_status}', ";
+        $query .= "post_image = '{$post_image}', ";
+        $query .= "post_content = '{$post_content}', ";
+        $query .= "post_tags = '{$post_tags}' ";
+        $query .= "WHERE post_id = {$the_post_id} ";
+
+        $update_post = mysqli_query($connection, $query);
+        confirm($update_post);
+    }
 }
 
 
@@ -27,7 +63,7 @@ if (isset($_GET['p_id'])) {
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
     <div class="form-group">
         <label for="title">Post Category</label>
@@ -52,7 +88,7 @@ if (isset($_GET['p_id'])) {
     </div>
     <div class="form-group">
         <label for="author">Post Author</label>
-        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
     <div class="form-group">
         <label for="post_status">Post Status</label>
@@ -60,6 +96,7 @@ if (isset($_GET['p_id'])) {
     </div>
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image ?>" alt="">
+        <input type="file" name="image">
     </div>
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
@@ -70,6 +107,6 @@ if (isset($_GET['p_id'])) {
         <input value="<?php echo $post_content; ?>" type="text" class="form-control" name="post_content">
     </div>
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
 </form>
