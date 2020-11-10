b71cc535ff899d3661271deb24c65fac80ff153a
Jake Dudum <jakedudum@gmail.com>
2020-11-09 19:23:21 -0800

    Modified view_all_users.php to display all users from the database.

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -1,62 +1,38 @@
 <table class="table table-bordered table-hover">
     <thead>
         <th>Id</th>
-        <th>Author</th>
-        <th>Comment</th>
+        <th>Username</th>
+        <th>Firstname</th>
+        <th>Lastname</th>
         <th>Email</th>
-        <th>Status</th>
-        <th>In Response to</th>
-        <th>Date</th>
-        <th>Approve</th>
-        <th>Unapprove</th>
-        <th>Delete</th>
+        <th>Role</th>
     </thead>
     <tbody>
         <?php
 
-        $query = "SELECT * FROM comments";
-        $select_comments = mysqli_query($connection, $query);
+        $query = "SELECT * FROM users";
+        $select_users = mysqli_query($connection, $query);
 
-        while ($row = mysqli_fetch_assoc(($select_comments))) {
-            $comment_id = $row['comment_id'];
-            $comment_post_id = $row['comment_post_id'];
-            $comment_author = $row['comment_author'];
-            $comment_content = $row['comment_content'];
-            $comment_email = $row['comment_email'];
-            $comment_status = $row['comment_status'];
-            $comment_date = $row['comment_date'];
+        while ($row = mysqli_fetch_assoc(($select_users))) {
+            $user_id = $row['user_id'];
+            $username = $row['username'];
+            $user_password = $row['user_password'];
+            $user_firstname = $row['user_firstname'];
+            $user_lastname = $row['user_lastname'];
+            $user_email = $row['user_email'];
+            $user_role = $row['user_role'];
+            $user_image = $row['user_image'];
 
             echo "<tr>";
-            echo "<td>$comment_id</td>";
-            echo "<td>$comment_author</td>";
-            echo "<td>$comment_content</td>";
-            echo "<td>$comment_email</td>";
-
-            // $query = "SELECT * FROM categories WHERE cat_id = {$comment_category_id} ";
-            // $select_categories_id = mysqli_query($connection, $query);
-
-            // while ($row = mysqli_fetch_assoc(($select_categories_id))) {
-            //     $cat_id = $row['cat_id'];
-            //     $cat_title = $row['cat_title'];
-            //     echo "<td>$cat_title</td>";
-            // }
-           
-            echo "<td>$comment_status</td>";
-
-            $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
-            $select_post_id_query = mysqli_query($connection, $query);
-
-            while($row = mysqli_fetch_assoc($select_post_id_query)) {
-                $post_id = $row['post_id'];
-                $post_title = $row['post_title'];
-
-                echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
-            }
-
-            echo "<td>$comment_date</td>";
-            echo "<td><a href='comments.php?approve={$comment_id}'>Approve</a></td>";
-            echo "<td><a href='comments.php?unapprove={$comment_id}'>Unapprove</a></td>";
-            echo "<td><a href='comments.php?delete={$comment_id}'>Delete</a></td>";
+            echo "<td>$user_id</td>";
+            echo "<td>$username</td>";
+            echo "<td>$user_firstname</td>";
+            echo "<td>$user_lastname</td>";
+            echo "<td>$user_email</td>";
+            echo "<td>$user_role</td>";
+            echo "<td><a href='users.php?approve={$user_id}'>Approve</a></td>";
+            echo "<td><a href='users.php?unapprove={$user_id}'>Unapprove</a></td>";
+            echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
             echo "</tr>";
         }
 
