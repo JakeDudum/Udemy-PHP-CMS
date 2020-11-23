7deafbde49cf5ebe74bdf06f40d6d589e46b4528
Jake Dudum <jakedudum@gmail.com>
2020-11-23 14:30:46 -0800

    Updated edit_post.php to have a dropdown menu for post_status instead of an input field.

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -91,8 +91,18 @@ if (isset($_GET['p_id'])) {
         <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
     <div class="form-group">
-        <label for="post_status">Post Status</label>
-        <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
+        <select name="post_status" id="">
+            <option value='<?php echo $post_status; ?>'><?php echo ucfirst($post_status); ?></option>
+            <?php
+
+            if($post_status == 'published') {
+                echo "<option value='draft'>Draft</option>";
+            } else {
+                echo "<option value='published'>Publish</option>";
+            }
+
+            ?>
+        </select>
     </div>
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image ?>" alt="">
