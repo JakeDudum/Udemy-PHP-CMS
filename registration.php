188f569fd6540ede80951cdb2d7b3245484068d5
Jake Dudum <jakedudum@gmail.com>
2021-01-13 19:33:11 -0800

    Added registration page to navbar in navigation.php and updated registration.php to submit form data todatabase and create new user.

--- registration.php
+++ registration.php
@@ -1,10 +1,40 @@
 <?php include "includes/db.php"; ?>
 <?php include "includes/header.php"; ?>
 
+<?php
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $email = mysqli_real_escape_string($connection, $email);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $query = "SELECT randSalt FROM users";
+    $select_randsalt_query = mysqli_query($connection, $query);
+
+    if(!$select_randsalt_query) {
+        die("Query Failed" . mysqli_error($connection));
+    }
+
+    $row = mysqli_fetch_array($select_randsalt_query);
+    $salt = $row['randSalt'];
+
+    $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
+    $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber')";
+    $register_user_query = mysqli_query($connection, $query);
+    if(!$register_user_query) {
+        die("Query Failed" . mysqli_error($connection) . ' ' . mysqli_errno($connection));
+    }
+}
+
+?>
+
 <!-- Navigation -->
 
 <?php include "includes/navigation.php"; ?>
-
 
 <!-- Page Content -->
 <div class="container">
