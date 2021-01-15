10c4306a4b9346a87f38e81860b720e33712324a
Jake Dudum <jakedudum@gmail.com>
2021-01-14 19:17:21 -0800

    Added the option to clone posts in view_all_posts.php and adjusted the table to display new posts first.

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,28 +1,52 @@
 <?php
 
-if(isset($_POST['checkBoxArray'])) {
+if (isset($_POST['checkBoxArray'])) {
 
-    foreach($_POST['checkBoxArray'] as $postValueId) {
+    foreach ($_POST['checkBoxArray'] as $postValueId) {
         $bulk_options = $_POST['bulk_options'];
 
-        switch($bulk_options) {
+        switch ($bulk_options) {
             case 'published':
                 $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId}";
                 $update_to_published_status = mysqli_query($connection, $query);
                 confirm($update_to_published_status);
-            break;
+                break;
 
             case 'draft':
                 $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId}";
                 $update_to_draft_status = mysqli_query($connection, $query);
                 confirm($update_to_draft_status);
-            break;
+                break;
 
             case 'delete':
                 $query = "DELETE FROM posts WHERE post_id = {$postValueId}";
                 $update_to_delete_status = mysqli_query($connection, $query);
                 confirm($update_to_delete_status);
-            break;
+                break;
+
+            case 'clone':
+                $query = "SELECT * FROM posts WHERE post_id = {$postValueId}";
+                $select_post_query = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_array($select_post_query)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_status = $row['post_status'];
+                    $post_image = $row['post_image'];
+                    $post_tags = $row['post_tags'];
+                    $post_content = $row['post_content'];
+                    $post_date = $row['post_date'];
+                }
+
+                $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
+                $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}')";
+                $copy_query = mysqli_query($connection, $query);
+
+                if (!$copy_query) {
+                    die("QUERY FAILED" . mysqli_error($connection));
+                }
+                break;
         }
     }
 }
@@ -37,6 +61,7 @@ if(isset($_POST['checkBoxArray'])) {
                 <option value="published">Publish</option>
                 <option value="draft">Draft</option>
                 <option value="delete">Delete</option>
+                <option value="clone">Clone</option>
             </select>
         </div>
         <div class="col-xs-4">
@@ -61,7 +86,7 @@ if(isset($_POST['checkBoxArray'])) {
         <tbody>
             <?php
 
-            $query = "SELECT * FROM posts";
+            $query = "SELECT * FROM posts ORDER BY post_id DESC";
             $select_posts = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc(($select_posts))) {
@@ -77,11 +102,11 @@ if(isset($_POST['checkBoxArray'])) {
 
                 echo "<tr>";
 
-                ?>
+            ?>
 
-                <td><input class='checkBoxes' type='checkbox' name="checkBoxArray[]" value="<?php echo $post_id;?>"></td>
+                <td><input class='checkBoxes' type='checkbox' name="checkBoxArray[]" value="<?php echo $post_id; ?>"></td>
 
-                <?php
+            <?php
                 echo "<td>$post_id</td>";
                 echo "<td>$post_author</td>";
                 echo "<td>$post_title</td>";
