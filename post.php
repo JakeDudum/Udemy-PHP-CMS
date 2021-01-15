23afdbe33b5e7989390f000ce71b7b352139c1c8
Jake Dudum <jakedudum@gmail.com>
2021-01-14 19:56:17 -0800

    Updated post and view_all_posts.php to handle new column for posts that tracks the amount of views a post has.

--- post.php
+++ post.php
@@ -16,39 +16,50 @@
 
             if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
-            }
 
-            $query = "SELECT * FROM posts WHERE post_id = {$the_post_id} ";
-            $select_all_posts_query = mysqli_query($connection, $query);
+                $view_query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id";
+                $send_query = mysqli_query($connection, $view_query);
 
-            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = $row['post_content'];
+                if(!$send_query) {
+                    die("Query Failed");
+                }
+
+                $query = "SELECT * FROM posts WHERE post_id = {$the_post_id} ";
+                $select_all_posts_query = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
 
             ?>
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#"><?php echo $post_title ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="author_posts.php?author=<?php echo $post_author ?>&p_id=<?php echo $the_post_id ?>"><?php echo $post_author ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
-                <hr>
-                <p><?php echo $post_content ?></p>
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#"><?php echo $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="author_posts.php?author=<?php echo $post_author ?>&p_id=<?php echo $the_post_id ?>"><?php echo $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content ?></p>
 
-            <?php } ?>
+            <?php }
+            } else {
+                header("Locations: index.php");
+            }
+
+            ?>
 
 
             <!-- Blog Comments -->
