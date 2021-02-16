2a1e32b9834a322cc6252826578a8a535af5da87
Jake Dudum <jakedudum@gmail.com>
2021-02-15 19:04:30 -0800

    Removed escape function from registration.php and copied its contents to create initial contact.php page.

--- contact.php
+++ contact.php
@@ -45,23 +45,21 @@ if (isset($_POST['submit'])) {
             <div class="row">
                 <div class="col-xs-6 col-xs-offset-3">
                     <div class="form-wrap">
-                        <h1>Register</h1>
+                        <h1>Contact</h1>
                         <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-                            <h6 class="text-center"><?php echo $message; ?></h6>
-                            <div class="form-group">
-                                <label for="username" class="sr-only">username</label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
-                            </div>
-                            <div class="form-group">
+                        <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email">
                             </div>
                             <div class="form-group">
-                                <label for="password" class="sr-only">Password</label>
-                                <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                                <label for="subject" class="sr-only">Subject</label>
+                                <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter your subject">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="body" id="body" cols="30" rows="10"></textarea>
                             </div>
 
-                            <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                            <input type="submit" name="submit" id="btn-submit" class="btn btn-custom btn-lg btn-block" value="Submit">
                         </form>
 
                     </div>
