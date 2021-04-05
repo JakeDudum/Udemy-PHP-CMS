bf3fa6752a5176381ce9680712c0cc1da679cc22
Jake Dudum <jakedudum@gmail.com>
2021-04-05 14:34:02 -0700

    Created delete_modal.php in admin which is a simple bootstrap modal being used to confirm post deletes. Also updated view_all_posts.php to use the new modal instead of browser confirm which required moving jquery link from admin_footer to admin_header.

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -44,6 +44,9 @@ if (!isset($_SESSION['user_role'])) {
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
     <script src="https://cdn.ckeditor.com/ckeditor5/24.0.0/classic/ckeditor.js"></script>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
 </head>
 
 <body>
