92f062808caea27f5d505273df5745de1365dd2b
Jake Dudum <jakedudum@gmail.com>
2021-04-27 14:45:58 -0700

    Added if else statement to db.php to use local db if there is no JAWSDB_URL.

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,26 @@
 <?php
 
 $url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
 
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
+if($url != null) {
+    $dbparts = parse_url($url);
 
-$connection = mysqli_connect($hostname, $username, $password, $database);
-
-?>
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+    
+    $connection = mysqli_connect($hostname, $username, $password, $database);
+    
+} else {
+    $db['db_host'] = "localhost";
+    $db['db_user'] = "root";
+    $db['db_pass'] = "";
+    $db['db_name'] = "cms";
+    
+    foreach($db as $key => $value) {
+        define(strtoupper($key), $value);
+    }
+    
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+}
